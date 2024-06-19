47e500ac1d27f55bf11fdbb67ed5c52f4623850c
msa-sadeghi <sadeghi.msai@gmail.com>
2024-06-19 16:53:57 +0330

    Update main.py

--- castle/main.py
+++ castle/main.py
@@ -16,9 +16,17 @@ bullet_group = pygame.sprite.Group()
 last_enemy_spawn_time = pygame.time.get_ticks()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+
+font = pygame.font.SysFont("arial", 48)
+next_level_text = font.render("WELCOME TO NEW LEVEL 1", True, (255, 20,10))
+next_level_rect = next_level_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 80))
 FPS = 60
+level = 1
+next_level = False
+level_reset_time = 0
 running = True
 while running:
+    screen.blit(bg, (0,0)) 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -28,9 +36,27 @@ while running:
             i = random.randrange(len(enemies_type))
             Enemy(enemies_type[i], enemies_health[i], 1, 100, random.choice([300,350,400,450,500,510]), enemy_group, 0.2)
             level_difficulty += enemies_health[i]    
+    else:
+        number_of_enemies = 0
+        
+        for enemy in enemy_group:
+            if enemy.alive:
+                number_of_enemies += 1
+        if number_of_enemies == 0 and next_level == False: 
+            next_level = True    
+            level_reset_time = pygame.time.get_ticks() 
+            
+            
+    if next_level:
+        next_level_text = font.render(f"WELCOME TO NEW LEVEL {level+1}", True, (255, 20,10))
+        screen.blit(next_level_text, next_level_rect) 
+        if pygame.time.get_ticks() - level_reset_time > 1500:
+            next_level = False
+            enemy_group.empty()
+            level += 1
+            max_difficulty = max_difficulty + 0.1 * max_difficulty
+            level_difficulty = 0
     
-    
-    screen.blit(bg, (0,0)) 
     castle.draw(screen) 
     castle.shoot(bullet_group) 
     bullet_group.draw(screen)     
